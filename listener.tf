fc2b75bc0ca4db6a17dcea42a37e61f420b801ee
Shravan <shravan.kumbar@gmail.com>
2022-11-12 13:26:23 +0530

    Added

---
+++ listener.tf
@@ -0,0 +1,17 @@
+# This creates a listener
+resource "aws_lb_listener" "private" {
+  count             =  var.INTERNAL ? 1 : 0  
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Okie"
+      status_code  = "200"
+    }
+  }
+}
